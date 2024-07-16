da7a95c5c6c2195bcc392855a1a6eaae3f8c1b1c
Lucas Valentin Montezuma <lucasvmontezuma@gmail.com>
2024-07-16 12:57:31 -0300

    implantando PostgreSQL

--- twitter/settings.py
+++ twitter/settings.py
@@ -66,10 +66,15 @@ WSGI_APPLICATION = 'twitter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tweeter',
+        'USER': 'lucas',
+        'PASSWORD': 'Lucas55418794*',
+        'HOST': 'localhost',  # ou o endereço do servidor PostgreSQL
+        'PORT': '5432',       # ou a porta usada pelo seu servidor PostgreSQL
     }
 }
+
 
 
 # Password validation
